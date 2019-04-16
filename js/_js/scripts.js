cc74a5fb9ad4f325043e3ce1c22d3ed6a879f06a
pavelp21 <pavelvp2182@yandex.ru>
2019-04-16 15:45:17 +0500

    Section Revs is domne

--- js/_js/scripts.js
+++ js/_js/scripts.js
@@ -1,49 +1,88 @@
 //@prepros-prepend jquery-2.1.1.min.js
 //@prepros-prepend bootstrap.min.js
+//@prepros-prepend jquery.fancybox.min.js
 
-$(document).ready(function() {
-/* Работа формы */
-$(function () {
-    var check = $('.check', this),
-        email = $('.email', this),
-        message = $('.alert_message', this),
-        button = $('.button_mail', this);
-    $(".form").on("submit", function () {
+$(document).ready(function () {
+    /* Работа формы */
+    $(function () {
         var check = $('.check', this),
-            message = $('.alert_message', this),
-            reNone = /.+/,
             email = $('.email', this),
+            message = $('.alert_message', this),
             button = $('.button_mail', this);
-        if (!email.val().match(reNone)) {
-            email.css({'borderColor':'red', 'transition':'all .4s ease'});
-            message.text('Введите email').slideDown(500);
-            return false;
-        }
-        if (!check.prop("checked")) {
-            check.next().css({'color':'red','transition':'all .4s ease'});
-            check.next().children().css({'color':'red','transition':'all .4s ease'});
-            message.text('Подтвердите соглашение').slideDown(500);
-            return false;
-        }
-        if (email.val() && check) {
-            // button.text('Отправляем...');
-            button.text('Отправляем...');
-            setTimeout(function () {
-                button.text('Отправлено!');
-            }, 500);
-        }
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert_message', this),
+                reNone = /.+/,
+                email = $('.email', this),
+                button = $('.button_mail', this);
+            if (!email.val().match(reNone)) {
+                email.css({
+                    'borderColor': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                // button.text('Отправляем...');
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
     });
-    email.click(function () {
-        // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
-        message.slideUp(500);
+    $(function () {
+        $("[data-fancybox]").fancybox({
+            buttons: [
+                'slideShow',
+                'share',
+                'zoom',
+                'fullScreen',
+                // 'download',
+                'close'
+            ],
+            speed: 330,
+            loop: true,
+            opacity: "auto",
+            // autoScale: true,
+            mouseWheel: true,
+            transitionEffect: 'slide'
+        });
     });
-    check.click(function () {
-        check.next().css({"color": "#000",'transition':'all .4s ease'});
-        check.next().children().css({"color": "#000",'transition':'all .4s ease'});
-        message.slideUp(500);
-    });
-});
+    $(function () {
 
-
-/*Конец документа*/
+        $(".video_wrapper img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1"  allowfullscreen></iframe>')
+        });
+    });
+    /*Конец документа*/
 });
